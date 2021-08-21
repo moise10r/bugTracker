35ba1deb20282d491bdc89472828d56e771f5071
moise10r <moiserushanika2006@gmail.com>
2021-08-21 21:43:25 +0200

    Add redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// import store from "./redux/store";
+import store from "./redux/store";
 import { bugAdded, bugRemoved, bugResolved } from './redux/actions';
-import customStore from "./redux/customStore";
+// import customStore from "./redux/customStore";
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log(store.getState());
-// });
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
-// store.dispatch(bugAdded('Bug 1'));
-// store.dispatch(bugAdded('Bug 2'));
-// store.dispatch(bugAdded('Bug 3'));
+store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugAdded('Bug 2'));
+store.dispatch(bugAdded('Bug 3'));
 // store.dispatch(bugResolved(2));
 // store.dispatch(bugResolved(1));
 // store.dispatch(bugResolved(3));
