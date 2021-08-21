dbbe4fe9b8f909a997ec077ca238befe38a0054e
moise10r <moiserushanika2006@gmail.com>
2021-08-21 16:10:11 +0200

    Dispatch an action

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-console.log('Hello world');
-import './style.css';
+import store from "./redux/store";
+
+store.dispatch({
+  type: 'ADD_BUG',
+  payload: {
+    description: 'Bug 1'
+  }
+});
+
+// store.dispatch({
+//   type: 'DELETE_BUG',
+//   payload: {
+//     id:1
+//   }
+// });
+console.log(store.getState());
