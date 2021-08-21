c1c885b0518aad9478d863f56a5d753cebb2da92
moise10r <moiserushanika2006@gmail.com>
2021-08-21 16:55:19 +0200

    Implement Bug resolved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import store from "./redux/store";
-import { bugAdded, bugRemoved } from './redux/actions';
+import { bugAdded, bugRemoved, bugResolved } from './redux/actions';
+
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(bugAdded('Bug 1'));
-store.dispatch(bugRemoved(1));
-unsubscribe();
+store.dispatch(bugAdded('Bug 2'));
+store.dispatch(bugAdded('Bug 3'));
+store.dispatch(bugResolved(2));
+store.dispatch(bugResolved(1));
+store.dispatch(bugResolved(3));
+// store.dispatch(bugRemoved(1));
+// unsubscribe();
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -12,6 +12,8 @@ const reducer = (state = [], action) => {
           resolved: false,  
         }
       ]
+      case actions.BUG_RESOLVED:
+        return state.map((bug) => bug.id !== action.payload.id ? bug : { ...bug, resolved = true});
       case actions.BUG_REMOVED:
         return state.filter((bug) => bug.id !== action.payload.id);
       
